5fcbafc3cfe11ce82734b69e7e1d8ea96e190e02
magechaP <magechapeter@gmail.com>
2019-03-05 17:35:08 +0300

    add professional UI to js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,42 @@ var playerroll = () => {
 
 
 //logic for professional fiasco
+var compscore = [];
+ var result = eval(playerscore.join('+'));
+  alert(result);
 
+  var playaroll = () => {
+    var num =Math.floor(Math.random()*6)+1;
+    if (num === 1){
+      //game ends for Player//
+      alert("1");
+      location.reload();
+      //no user score dont push to userscore array;
+  }
+    else
+    {
+
+      userscore.push(num);
+      var give = document.getElementById('usercount');
+      give.innerHTML += "<li>"+num+"</li>";
+      userresult();
+    }
+
+    }
 
 //UserInterface//
+//UI for amateur level//
 $(document).ready(function(){
  $(".roll").on("click",function(){
+   playerroll();
+ });
+
+});
+
+
+//UI for professional fiasco
+$(document).ready(function(){
+ $(".coms").on("click",function(){
    playerroll();
  });
 
